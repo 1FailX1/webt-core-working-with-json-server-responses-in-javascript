c3d1c3c8eedf6b6155ecfbcd9206ad7b0bb089cf
Felix Payer <1felixpayer1@gmail.com>
2023-11-29 16:01:57 +0100

    Added vue router functinoality

---
+++ json-server-responses-in-javascript/src/components/Homepage.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { ref } from 'vue';
+
+const sampleData = ref('')
+logMovies()
+
+async function logMovies() {
+  const response = await fetch('https://jsonplaceholder.typicode.com/todos/1')
+  response.json().then((data) => {
+    sampleData.value = data
+  })
+}
+
+</script>
+
+<template>
+    <p>{{ sampleData }}</p>
+  </template>
+  
