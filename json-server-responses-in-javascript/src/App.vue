37250c95fbe188b681b26757c14981e4bc10ca93
Felix Payer <1felixpayer1@gmail.com>
2023-11-29 16:03:45 +0100

    Merged dev branch to main

--- json-server-responses-in-javascript/src/App.vue
+++ json-server-responses-in-javascript/src/App.vue
@@ -1,18 +1,13 @@
 <script setup>
-import { ref } from 'vue';
-
-const sampleData = ref('')
-logMovies()
-
-async function logMovies() {
-  const response = await fetch('https://jsonplaceholder.typicode.com/todos/1')
-  response.json().then((data) => {
-    sampleData.value = data
-  })
-}
-
 </script>
 
 <template>
-  <p>{{ sampleData }}</p>
+  <div>
+    <router-link to="/">Homepage</router-link> |
+    <router-link to="/US1">UserStory1</router-link> |
+    <router-link to="/US2">UserStory2</router-link> |
+    <router-link to="/US3">UserStory3</router-link> |
+    <router-link to="/US4">UserStory4</router-link>
+    <router-view />
+  </div>
 </template>
